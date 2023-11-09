f4a38e1587c34dc11852e5cd9bef334a2940fd2e
Keeanu Rodriguez <keeanu7@gmail.com>
2023-11-09 16:26:14 -0500

    set up routes

---
+++ App.jsx
@@ -0,0 +1,35 @@
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from "./src/pages/Home.jsx";
+import Inventory from "./src/pages/Inventory.jsx";
+import SignUp from "./src/pages/SignUp.jsx";
+import LogIn from "./src/pages/LogIn.jsx";
+import Recipes from "./src/pages/Recipes.jsx";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/inventory" element={<Inventory/>}/>
+        <Route path="/signup" element={<SignUp/>}/>
+        <Route path="/login" elemetn={<LogIn/>}/>
+        <Route path="/recipes" element={<Recipes/>}/>
+      </Routes>
+    </BrowserRouter>
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
