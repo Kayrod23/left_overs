4a1bcafbc21da5c461ad301489bbadf31264d92d
Keeanu Rodriguez <keeanu7@gmail.com>
2024-01-06 22:10:15 -0500

    works with uploaded images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,39 +13,13 @@ const openai = new OpenAI({
 // console.log(openai);
 
 function Home () {
-  const [imageUrl, setImageUrl] = useState("");
+  // const [imageUrl, setImageUrl] = useState("");
   const [message, setMessage] = useState("");
   const [recipeSteps, setRecipeSteps] = useState("");
 
-  // useEffect(() => {
-  //   async function imageTest () {
-  //     const response = await openai.chat.completions.create({
-  //       model: "gpt-4-vision-preview",
-  //       messages: [
-  //         {
-  //           role: "user",
-  //           content: [
-  //             { type: "text", text: "Generate a recipe, list recipe name only" },
-  //             {
-  //               type: "image_url",
-  //               image_url: {
-  //                 "url": "https://static9.depositphotos.com/1228594/1079/i/450/depositphotos_10795872-stock-photo-milkbutter-and-eggs-as-dairy.jpg",
-  //               },
-  //             },
-  //           ],
-  //         },
-  //       ],
-  //     });
-  //     console.log(response.choices[0].message.content);
-  //     setMessage(response.choices[0].message.content);
-  //     // send this to the back end along with the steps when completed
-  //   }
-  //   //imageTest();
-  // }, [])
-
   // takeImageInput takes an image file from your computer and send it to imgbb to be hosted so the image now has a url linked to it.
   // this allows it to be sent to chatgpt and analyzed to create a recipe.
-  function takeImageInput(event) {
+  function takeImageInputAndSendToOpenAI(event) {
     event.preventDefault();
     const form = new FormData(event.target);
     fetch(`https://api.imgbb.com/1/upload?expiration=600&key=${IMGBB_API}`, {
@@ -59,16 +33,70 @@ function Home () {
         return response.json();
       })
       .then(data => {
-        setImageUrl(data.data.url)
+        async function imageTest (imageUrl) {
+          console.log(2)
+          const response = await openai.chat.completions.create({
+            model: "gpt-4-vision-preview",
+            messages: [
+              {
+                role: "user",
+                content: [
+                  { type: "text", text: "Generate a recipe, list recipe name only" },
+                  {
+                    type: "image_url",
+                    image_url: {
+                      "url": `${data.data.url}`,
+                    },
+                  },
+                ],
+              },
+            ],
+          });
+          console.log(response.choices[0].message.content);
+          setMessage(response.choices[0].message.content);
+          // send this to the back end along with the steps when completed
+        }
+        imageTest();
       })
       .catch(error => {
         console.error('Error uploading image:', error);
       });
-  }
-  console.log(imageUrl);
-  // useEffect(() => {
 
-  // }, [])
+      //run showChatGPTUserImage() if imageUrl is updated
+      console.log("here", imageUrl)
+      // if (imageUrl) {
+      //   // useEffect(() => {
+      //     console.log(1)
+      //     async function imageTest (imageUrl) {
+      //       console.log(2)
+      //       const response = await openai.chat.completions.create({
+      //         model: "gpt-4-vision-preview",
+      //         messages: [
+      //           {
+      //             role: "user",
+      //             content: [
+      //               { type: "text", text: "Generate a recipe, list recipe name only" },
+      //               {
+      //                 type: "image_url",
+      //                 image_url: {
+      //                   "url": `${imageUrl}`,
+      //                 },
+      //               },
+      //             ],
+      //           },
+      //         ],
+      //       });
+      //       console.log(response.choices[0].message.content);
+      //       setMessage(response.choices[0].message.content);
+      //       // send this to the back end along with the steps when completed
+      //     }
+      //     imageTest();
+      //   // }, [])
+      //}
+  }
+
+  // console.log(imageUrl);
+  console.log(message);
   return (
     <div style={styles.container}>
       <NavBar/>
@@ -78,7 +106,7 @@ function Home () {
         <p style={styles.recipeSteps}>{recipeSteps ? recipeSteps : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."}</p>
       </div>
       : 
-        <form style={styles.form} onSubmit={takeImageInput}>
+        <form style={styles.form} onSubmit={takeImageInputAndSendToOpenAI}>
           {/* <label htmlFor='file'>Image</label> */}
           <input id="file" type="file" accept="image/*" capture="camera" name="image"/>
           <button type="submit">Submit</button>
